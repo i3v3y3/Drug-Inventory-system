2a275fe3c9b8f8b9cea7b629589981e1286365a7
i3v3y3 <62935803+i3v3y3@users.noreply.github.com>
2024-03-04 19:12:49 +0300

    This is my final two codes

--- Maintake.cpp
+++ Maintake.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include "Procu.h"
 
 #include <mysql_driver.h>
 #include <mysql_connection.h>
@@ -13,10 +14,9 @@ using namespace sql;
 using namespace std;
 
 
-string drugname, availabledrug, drugbrand;
+string drugname, drugbrand;
 
-int quantity, availableno, buyingno;
-
+int quantity, availableno, buyingno, newnum, availabledrug, NO_;
 
 
 
@@ -42,7 +42,7 @@ int main()
     cin >> usersname;
 
     //I will now perform a username check after I get data on who is the user
-    if (usersname == "Doc" || usersname == "Pharmacy" || usersname == "Desk" || usersname == "Daktari")
+    if (usersname == "Doc" || usersname == "Pharmacy" || usersname == "Front Desk" || usersname == "Daktari") // You can now include whoever is in charge of selling or checking
     {
 
 
@@ -70,39 +70,81 @@ int main()
             res = stmt->executeQuery("SELECT * FROM drugsavailable");
 
             while (res->next()) {
-                int quantity = res->getInt(1);
-                string drugname = res->getString(2);
-                string drugbrand = res->getString(3);
-                cout << "\t\t " << quantity << " \t\t" << drugname << " \t\t " << drugbrand << endl;
+                int NO_ = res->getInt(1);
+                int quantity = res->getInt(2);
+                string drugname = res->getString(3);
+                string drugbrand = res->getString(4);
+                cout << "\t\t "<<NO_<<"\t\t" << quantity << " \t\t" << drugname << " \t\t " << drugbrand << endl;
             }
             //Here is the selection prompt which can be made easier by just selection but we use input prompt
+            cout << "\t\t\n\n Do you want to sell a specific drug?\n\n\t\t Enter the number of the drug:";
 
-            cout << "\t\t\n\n Do you want to sell a specific drug?\n\n\t\t Enter the name of the drug:";
-
+            int availabledrug;
             cin >> availabledrug;
+
+            // Get the current quantity of the selected drug
+            res = stmt->executeQuery("SELECT quantity FROM drugsavailable WHERE NO_ = " + std::to_string(availabledrug));
+            int currentQuantity = 0;
+            if (res->next()) {
+                currentQuantity = res->getInt(1);
+            }
+            else {
+                cout << "Drug not found." << endl;
+                delete res;
+                delete stmt;
+                delete con;
+                return 1;
+            }
+
             cout << "\t\t\n\nEnter the number available : ";
 
+            
             cin >> availableno;
+
             cout << "\n\n\t\t How many does the client want\t\t\n\n Enter here:";
 
+           
             cin >> buyingno;
-            while (availableno == quantity && availabledrug == drugname)
-            {
-                sql::PreparedStatement* pstmt;
-                pstmt = con->prepareStatement("UPDATE drugsavailable SET quantity = quantity - buyingno");
-                pstmt->setInt(1, quantity);
-                pstmt->executeUpdate();
 
+            if (buyingno <= 0 || buyingno > availableno) {
+                cout << "Invalid input." << endl;
+            }
+            else if (currentQuantity >= buyingno) {
+                // Subtract the buyingno from the currentQuantity
+                currentQuantity -= buyingno;
+
+                // Update the quantity column in the database
+                sql::PreparedStatement* pstmt;
+                pstmt = con->prepareStatement("UPDATE drugsavailable SET quantity = ? WHERE NO_ = ?");
+                pstmt->setInt(1, currentQuantity);
+                pstmt->setInt(2, availabledrug);
+                pstmt->executeUpdate();
+                Sleep(1000);
                 cout << "\n Successfully bought." << endl;
 
+                system("cls");
+                Sleep(5000);
 
+                // Display updated table
+                res = stmt->executeQuery("SELECT * FROM drugsavailable");
+                cout << "\n\n Updated Table:\n\n";
+                cout << "\t\t NO_\t\t Quantity\t\t Drug Name\t\t Drug Brand\n";
+                while (res->next()) {
+                    int NO_ = res->getInt(1);
+                    int quantity = res->getInt(2);
+                    string drugname = res->getString(3);
+                    string drugbrand = res->getString(4);
+                    cout << "\t\t " << NO_ << "\t\t" << quantity << " \t\t" << drugname << " \t\t " << drugbrand << endl;
+                }
+            }
+            else {
+                cout << "Not enough quantity available." << endl;
             }
 
             delete res;
             delete stmt;
             delete con;
-
-
+         
         }
         catch (sql::SQLException& e) {
             cout << "# ERR: SQLException in " << __FILE__;
@@ -111,10 +153,18 @@ int main()
             cout << " (MySQL error code: " << e.getErrorCode();
             cout << ", SQLState: " << e.getSQLState() << " )" << endl;
         }
-
-
-
-
         return 0;
-    };
+    }
+    else {
+      cout<<"\n\n\t\t Are you in procument? ";
+      string procpage;
+      cin>> procpage;
+      if (procpage == "yes" || procpage == "Y" || procpage == "YES"|| procpage=="Yes") {
+        checkpoint();
+      }
+      else {
+          cout << "Invalid choice!! Kindly reload the page";
+      }
+    }
+    return (1);
 }
---
+++ Procu.h
@@ -0,0 +1,94 @@
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace std;
+
+int newnodrug;
+
+void checkpoint()
+{
+    system("cls");
+    Sleep(1000);
+
+    cout << "\t\t\t\t\t\t\n\n\n\n Welcome Procument!!";
+    cout << "\t\t\n\nThe system will now load the drugs from the database. Please be patient...";
+    system("cls");
+
+    sql::Driver* driver;
+    sql::Connection* con;
+    driver = get_driver_instance();
+    // here change your username and password as intended
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "MatoborIvy33");
+
+    con->setSchema("druginventory");
+
+    sql::Statement* stmt;
+    stmt = con->createStatement();
+
+    sql::ResultSet* res;
+    res = stmt->executeQuery("SELECT * FROM drugsavailable");
+
+    while (res->next()) {
+        int NO_ = res->getInt(1);
+        int quantity = res->getInt(2);
+        string drugname = res->getString(3);
+        string drugbrand = res->getString(4);
+        cout << "\t\t " << NO_ << "\t\t" << quantity << " \t\t" << drugname << " \t\t " << drugbrand << endl;
+
+    }
+    cout << "What would you like to perform?\n\n\t 1.Restock on drugs\n\n\t 2. Remove specific drug\n\n\t\t";
+
+    int procchoice;
+    cin >> procchoice;
+    switch (procchoice)
+    {
+    case 1:
+        system("cls");
+        Sleep(1000);
+       
+
+        sql::ResultSet* res = stmt->executeQuery("SELECT * FROM drugsavailable WHERE NO_ = " + std::to_string(newnodrug));
+        if (!res->next()) {
+            cout << "Drug not found." << endl;
+            delete res;
+            break;
+        }
+
+        cout << "\t\t\n\n How many are you adding?: ";
+        int newnumber;
+        cin >> newnumber;
+
+        // Prepare a statement to update the quantity column in the table
+        sql::PreparedStatement* pstmt;
+        pstmt = con->prepareStatement("UPDATE drugsavailable SET quantity = quantity + ? WHERE NO_ = ?");
+        pstmt->setInt(1, newnumber);
+        pstmt->setInt(2, newnodrug);
+
+        // Execute the statement and update the table
+        int rowsAffected = pstmt->executeUpdate();
+        cout << rowsAffected << " row(s) updated." << endl;
+
+        // Close the prepared statement
+        delete pstmt;
+
+        Sleep(1000);
+
+        break;
+    case 2:
+        // you can write here something else you want to be performed
+
+        break;
+
+    }
+}
